aab14fe4639e6347361193bf4f12bfb237526da1
Srikanth Pragada <srikanthpragada@gmail.com>
2016-03-15 08:13:07 +0530

    Display department with most experienced employee

--- deptname_highest_exp_employee.sql
+++ deptname_highest_exp_employee.sql
@@ -36,3 +36,33 @@ begin
 
 end;  
  
+With Cursor For Loop
+====================
+set serveroutput on
+declare
+  cursor deptcur is
+    select department_id, department_name
+    from departments
+    where department_id in (select department_id from employees)
+    order by 2;
+   v_name     employees.first_name%type;
+begin
+  for deptrec in deptcur
+  loop
+     begin 
+      -- get employee with hightest experience in this dept
+      select first_name into v_name
+      from   employees
+      where  department_id =  deptrec.department_id
+      and hire_date = 
+       (select min(hire_date)  from employees 
+        where department_id =  deptrec.department_id );
+        
+      dbms_output.put_line(deptrec.department_name || ' '  || v_name);
+     exception 
+       when too_many_rows then
+          dbms_output.put_line(deptrec.department_name || ' is having more than one employee');   
+     end;
+   end loop;
+end;  
+ 
