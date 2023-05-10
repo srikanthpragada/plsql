68fdbe57c45c718b3ad31e2c1e84529d8c1e868f
rafalm11 <133100213+rafalm11@users.noreply.github.com>
2023-05-10 12:39:56 +0200

    reduce redundant salary runs
    the change reduces redundant employees table scans from 4 to just 1.
    It uses analytical function to calculate average and then uses this value in a single table scan (instead of a separate 4 runs)

--- salary_stats.sql
+++ salary_stats.sql
@@ -1,25 +1,19 @@
 -- Display no. of employees with salary > avg, < avg and = avg
 set serveroutput on 
 declare
-   v_avg_salary employees.salary%type;
    v_high number(2);
    v_same number(2);
    v_low number(2);
 begin
-   select avg(salary) into v_avg_salary
-   from employees;
    
-   select count(*) into v_high
-   from employees
-   where salary > v_avg_salary;
-   
-   select count(*) into v_same
-   from employees
-   where salary = v_avg_salary;
-   
-   select count(*) into v_low
-   from employees
-   where salary < v_avg_salary;
+   select count(case when salary > avg_salary then 1 end), --more than average
+          count(case when salary = avg_salary then 1 end), --the same as average
+          count(case when salary < avg_salary then 1 end), --less than average
+          into v_high, v_same, v_low
+   from (
+         select salary, avg(salary) over (partition by 1) avg_salary
+         from employees
+        );
    
    dbms_output.put_line('Higher than avg : ' || v_high);
    dbms_output.put_line('Same as avg     : ' || v_same);
